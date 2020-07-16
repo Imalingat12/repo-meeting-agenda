9a6e72e46e78944d93aa7936ae19f860d95f8da4
Imalingat12 <66953519+Imalingat12@users.noreply.github.com>
2020-07-16 23:47:50 +0300

    added my meeting agenda

---
+++ JS/index.js
@@ -0,0 +1,68 @@
+
+// deigning the word date by increasing its fontsize and making it bold
+var x =document.getElementById("date")
+x.style.fontSize ="20px";
+x.style.fontWeight ="bold";
+
+// deigning the day of the date by making it a white in color and adding its fontsize
+var x =document.getElementById("dt")
+x.style.color = "white";
+x.style.fontSize ="15px";
+
+// designing the word time by increasing its fontsize and making it bold
+var z =document.getElementById("time")
+z.style.fontSize ="20px";
+z.style.fontWeight ="bold";
+
+// designing the hours and minutes of time by making it a white in color and adding its fontsize 
+var y =document.getElementById("minhr")
+y.style.color = "white";
+y.style.fontSize = "15px";
+
+// designing the word venue by increasing its fontsize and making it bold
+var z =document.getElementById("venue")
+z.style.fontSize ="20px";
+z.style.fontWeight ="bold";
+
+// designing the room of a venue by making it a white in color and adding its fontsize 
+var y =document.getElementById("vnu")
+y.style.color = "white";
+y.style.fontSize = "15px";
+
+// designing topic by changing to black in color with bold and decreasing it fontsize.
+var w = document.getElementById("point1")
+w.style.fontSize ="13px";
+w.style.color ="black";
+
+// designing topic by changing to black in color with bold and decreasing its fontsize.
+var w = document.getElementById("point2")
+w.style.fontSize ="13px";
+w.style.color ="black";
+
+// designing topic by changing to black in color with bold and decreasing its fontsize.
+var w = document.getElementById("point3")
+w.style.fontSize ="13px";
+w.style.color ="black";
+
+// designing topic by changing to black in color with bold and decreasing its fontsize.
+var w = document.getElementById("point4")
+w.style.fontSize ="13px";
+w.style.color ="black";
+
+// designing topic by changing to black in color with bold and decreasing its fontsize.
+var w = document.getElementById("point5")
+w.style.fontSize ="13px";
+w.style.color ="black";
+
+// designing topic by changing to black in color with bold and decreasing its fontsize.
+var w = document.getElementById("point6")
+w.style.fontSize ="13px";
+w.style.color ="black";
+
+// designing the footer by changing its color to blue with bold and increasing its fontsize
+var w = document.getElementById("nts")
+w.style.fontSize ="25px";
+w.style.color ="blue";
+w.style.fontWeight ="bold";
+
+
